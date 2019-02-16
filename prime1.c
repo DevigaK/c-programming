d1424a8379df7cd3ef4e8f8c6ab1800b450e35af
DevigaK <47448073+DevigaK@users.noreply.github.com>
2019-02-16 16:32:42 +0530

    Create prime1.c

---
+++ prime1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+   int i, l,r, p = 0,k,count=0;
+      scanf("%d %d", &l,&r);
+      k=l;
+      while(k<=r)
+      {
+          p=0;
+   for(i=1; i<=k; i++)
+   {
+      if(k%i==0)
+      {
+         p++;
+      }
+   }
+   if(p==2)
+   {
+      count=count+1;
+   }
+   k++;
+      
+      }
+      
+      printf("%d",count);
+      return 0;
+}
